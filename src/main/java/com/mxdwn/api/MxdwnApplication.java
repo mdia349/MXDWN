b1b6f0e85c75dd2f300e7b714d91dda35af82348
Matthew Diaz <mdia349@wgu.edu>
2026-05-24 07:04:35 -0700

    Initial commit

---
+++ src/main/java/com/mxdwn/api/MxdwnApplication.java
@@ -0,0 +1,13 @@
+package com.mxdwn.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MxdwnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MxdwnApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mxdwn/api/MxdwnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mxdwn.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MxdwnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
