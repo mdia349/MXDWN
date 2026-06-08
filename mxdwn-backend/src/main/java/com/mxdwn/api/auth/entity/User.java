6cc78feb1131b73735790f78021df5410ff86631
mdia <mdia349@wgu.edu>
2026-06-07 19:06:45 -0700

    - Beginning auth implementation
    - Added user entity

---
+++ mxdwn-backend/src/main/java/com/mxdwn/api/auth/entity/User.java
@@ -0,0 +1,59 @@
+package com.mxdwn.api.auth.entity;
+
+import jakarta.persistence.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.UUID;
+
+@Entity
+@Table(name = "users")
+public class User implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    private String displayName;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    // ---------------- //
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_" + role.name()));
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() { return true; }
+
+    @Override
+    public boolean isAccountNonLocked() { return true; }
+
+    @Override
+    public boolean isCredentialsNonExpired() { return  true; }
+
+    @Override
+    public boolean isEnabled() { return true; }
+}
